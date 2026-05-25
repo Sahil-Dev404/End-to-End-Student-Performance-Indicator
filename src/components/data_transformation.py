35bc704465c2115bdee16d3e9a5f476016b0d46b
Sahil-Dev404 <2005sahilsaini@gmail.com>
2026-05-25 21:19:56 +0530

    small corection

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -62,7 +62,7 @@ class DataTransformation:
     def initiate_data_transformation(self,train_path,test_path):
         try:
             train_df=pd.read_csv(train_path)
-            test_df=df.read_csv(test_path)
+            test_df=pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
             logging.info("Obtaining preprocessing object")
