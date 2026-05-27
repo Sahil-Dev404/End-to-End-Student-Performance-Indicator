339d50d77722f6d2c5122fe2dc177c6cc6a2ac69
Sahil-Dev404 <2005sahilsaini@gmail.com>
2026-05-27 18:14:16 +0530

    bugs fixed in the prediction pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys 
+import os
 import pandas as pd 
 from src.exception import CustomException
 from src.utils import load_object
@@ -25,7 +26,7 @@ class PredictPipeline:
         
 
 class CustomData:
-    def __init__(self,gender:str,race_ethnicity:str,parental_level_of_eduaction,lunch:str,test_preparation_course:str,reading_score:int,writing_score:int):
+    def __init__(self,gender:str,race_ethnicity:str,parental_level_of_education:str,lunch:str,test_preparation_course:str,reading_score:int,writing_score:int):
         self.gender = gender
 
         self.race_ethnicity = race_ethnicity
